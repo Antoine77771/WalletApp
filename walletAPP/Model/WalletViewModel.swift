8242af054f5b8492921c39532c9737d309a01921
Antoine <antoineguerregenton@gmail.com>
2022-04-28 12:32:06 +0200

    first commit

---
+++ walletAPP/Model/WalletViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WalletViewModel.swift
+//  walletAPP
+//
+//  Created by Antoine on 26/04/2022.
+//
+
+import Foundation
---
+++ walletAPP/View/ParameterView.swift
@@ -0,0 +1,20 @@
+//
+//  ParameterView.swift
+//  walletAPP
+//
+//  Created by Antoine on 26/04/2022.
+//
+
+import SwiftUI
+
+struct ParameterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ParameterView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParameterView()
+    }
+}
